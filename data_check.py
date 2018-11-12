601c9d8eb47a8d6e55f351e608df7bdac5ae52cd
LittleRabbitHole <li.ang198744@gmail.com>
2018-11-11 21:44:30 -0500

    commit

--- data_check.py
+++ data_check.py
@@ -9,4 +9,5 @@ Created on Sun Nov 11 21:15:47 2018
 import pandas as pd
 
 data = pd.read_table("/Users/jiajunluo/OneDrive/Documents/Pitt_PhD/ResearchProjects/WikiWorldEvent/data/all_contributors.txt", 
-                             sep='||', error_bad_lines = False)
+                             sep='\\|\\|', error_bad_lines = False)
+
