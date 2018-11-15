6226bafcb1db864ea5b49e1c6b5da127b3dbc247
Angrrr_ <li.ang198744@gmail.com>
2018-11-15 17:23:43 -0500

    commit

--- WikiReverts.py
+++ WikiReverts.py
@@ -8,6 +8,7 @@ use of mwreverts library
 http://paws-public.wmflabs.org/paws-public/User:EpochFail/editquality/ipython/reverted_detection_demo.ipynb
 
 This includes function of how to identify revert given a (language, revid, pageid)
+https://pythonhosted.org/mwreverts/
 
 @author: angli
 """
@@ -16,11 +17,17 @@ This includes function of how to identify revert given a (language, revid, pagei
 
 import mwapi, mwreverts.api
 
-# Gather a page's revisions from the API
-session = mwapi.Session("https://en.wikipedia.org", user_agent="mwreverts basic usage script")
+
+def RevertDetect(lang, rev_id, page_id)
+    # Gather a page's revisions from the API
+    Wiki_link = "https://{}.wikipedia.org".format(lang)
+    session = mwapi.Session(Wiki_link, user_agent="mwreverts basic usage script")
+    reverting, reverted, reverted_to = mwreverts.api.check(session, rev_id, page_id, radius=20, rvprop={'user','userid'})
+
 
 
 #An edit can reverting other edits, it can be reverted, or it can be reverted_to by another edit.
+    
 def print_revert_status(rev_id, reverting, reverted, reverted_to):
     """Prints a nice, pretty version of a revert status."""
     print(str(rev_id) + ":")
@@ -31,7 +38,10 @@ def print_revert_status(rev_id, reverting, reverted, reverted_to):
     if reverted_to is not None:
         print(" - reverted_to in {revid} by {user}".format(**reverted_to.reverting))
 
-reverting, reverted, reverted_to = mwreverts.api.check(session, rev_id=423282501, page_id=31445634, radius=20, rvprop={'user','userid'})
+session = mwapi.Session("https://en.wikipedia.org", user_agent="mwreverts basic usage script")
+reverting, reverted, reverted_to = mwreverts.api.check(session, rev_id=695071713 , #page_id=31445634, 
+                                                       radius=20, window=48*60*60,  # 2 days
+                                                       rvprop={'user','userid', 'ids', 'size'})
 
 print_revert_status(423282501, reverting, reverted, reverted_to)
 
